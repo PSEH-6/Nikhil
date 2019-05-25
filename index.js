460da539599e2c64376ae1549b6fc259a28ab59f
India Hiring <indiahiring@global.publicisgroupe.net>
2019-05-25 13:27:59 +0530

    Added log4j

--- index.js
+++ index.js
@@ -2,14 +2,18 @@
 const express = require('express')
 const processor = require('./service/weatherApiProcessor')
 const app = express()
-var convert = require('xml-js');
-var request = require('request');
+const convert = require('xml-js');
+const request = require('request');
+const log4js = require('log4js');
+const logger = log4js.getLogger();
 const port = process.env.PORT
 
 var router = express.Router();
 
 /* GET users listing. */
 app.get('/api/v1/weather/predict', function(req, res) {
+
+    logger.info('Inside api method')
 
     request({
         uri: "https://samples.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml&appid=b6907d289e10d714a6e88b30761fae22",
@@ -19,24 +23,27 @@ app.get('/api/v1/weather/predict', function(req, res) {
         maxRedirects: 10
       }, function(error, response, body) {
           if (!error && response.statusCode === 200) {
+            logger.info('openweathermap api call is success')
             var result = convert.xml2json(body, {compact: true, spaces: 4});
             json_response = new Object()
             let message = predictWeather(body) ;
 
             // Populate API response based on climate prediction
-            if(message == "WARM"){
-                console.log('WARM');
+            if(message == "HOT"){
+                logger.info('Prediction result : HOT')
+
                 res.setHeader('Content-Type', 'application/json');
                 res.end(JSON.stringify({ code: 200, status :'Success',message : "Use sunscreen lotion" }));
             }
             else if(predicatWeather(body) == "COLD") 
             {
-                console.log('COLD');
+                logger.info('Prediction result : HOT')
                 res.setHeader('Content-Type', 'application/json');
                 res.end(JSON.stringify({ code: 200, status :'Success',message : "Carry umbrella" }));
             }
 
           } else {
+            logger.info('openweathermap api call is failure')
             res.status(500);
             return next(err);
           }
@@ -46,7 +53,7 @@ app.get('/api/v1/weather/predict', function(req, res) {
 // analyse data from API response and populate message
 function predictWeather(body){
     console.log('ReachedpredictWeather');
-    return "WARM";
+    return "HOT";
 
 }
 
